9469ddad8865cae8858802e1ce16ea4df81b5faa
nellysme <nelly1@live.no>
2022-11-13 21:30:00 +0100

    6y6
    66

---
+++ script.js
@@ -0,0 +1,69 @@
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+    
+];
+
+
+// Question 1
+
+var cat = {
+    complain(Meow) {
+        console.log("meow")
+    },
+}
+
+
+
+console.log("meow");
+
+
+// Question 2
+
+const headning = document.querySelector("h3");
+headning.innerHTML = "Updated headning";
+
+
+// Question 3
+
+headning.style.fontSize = "2em";
+
+// Question 4
+
+headning.classList.add("subheadning");
+
+// Question 5
+
+const paragraphs = document.querySelectorAll("p");
+
+for(let i = 0; i < paragraphs.length; i++){
+    paragraphs[i].style.color = "red";
+}
+// Question 6
+
+const resultsContainer = document.querySelector("div");
+resultsContainer.innerHTML = "New Paragraph";
+resultsContainer.style.backgroundColor = "yellow"
+
+// Question 7
+
+function name(list){
+    var list = ("list")
+    console.log(name)
+}
+
+console.log(cats)
+
+// Question 8
+
+// not sure
+
